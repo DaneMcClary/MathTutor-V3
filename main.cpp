e26790edc71683a35974f7b5f9dcb53fa03a8b18
JD-4450 <jacob.diedrichsen4450@gmail.com>
2025-10-08 11:59:09 -0500

    Fixed the numerical only answer while loop.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@
 #include <cmath>
 #include <cstdlib>
 #include <cctype>
+#include <ios>
+#include <limits>
 #include <random>
 
 
@@ -37,104 +39,138 @@ int main() {
 
 
     //   /\  Input any new values here please!
-        cout <<   "*************************************************************" << endl;
-        cout <<   " __  __       _   _       __  __       _   _                 " << endl;
-        cout <<   "|  \/  | __ _| |_| |__   |  \/  | __ _| |_| |_ ___ _ __ ___  " << endl;
-        cout <<   "| |\/| |/ _` | __| '_ \  | |\/| |/ _` | __| __/ _ \ '__/ __| " << endl;
-        cout <<   "| |  | | (_| | |_| | | | | |  | | (_| | |_| ||  __/ |  \__ \ " << endl;
-        cout <<   "|_|  |_|\__,_|\__|_| |_| |_|  |_|\__,_|\__|\__\___|_|  |___/ " << endl;
-        cout <<   " _____      _              __     _______                    " << endl;
-        cout <<   "|_   _|   _| |_ ___  _ __  \ \   / /___ /                    " << endl;
-        cout <<   "  | || | | | __/ _ \| '__|  \ \ / /  |_ \                    " << endl;
-        cout <<   "  | || |_| | || (_) | |      \ V /  ___) |                   " << endl;
-        cout <<   "  |_| \__,_|\__\___/|_|       \_/  |____/                    " << endl;
-        cout <<   "*************************************************************" << endl;
-        cout << "Math is good for your brain." << endl;
-        cout << "So lets feed your brain some math!" << endl << endl;
-        cout << "What is your name my pupil? :";
-        getline(cin, name); // gets the users name
-        cout << "Hello ";
-        cout << name << ". Lets learn some math!!!" << endl;
+    cout <<   "*************************************************************" << endl;
+    cout <<   " __  __       _   _       __  __       _   _                 " << endl;
+    cout <<   "|  \/  | __ _| |_| |__   |  \/  | __ _| |_| |_ ___ _ __ ___  " << endl;
+    cout <<   "| |\/| |/ _` | __| '_ \  | |\/| |/ _` | __| __/ _ \ '__/ __| " << endl;
+    cout <<   "| |  | | (_| | |_| | | | | |  | | (_| | |_| ||  __/ |  \__ \ " << endl;
+    cout <<   "|_|  |_|\__,_|\__|_| |_| |_|  |_|\__,_|\__|\__\___|_|  |___/ " << endl;
+    cout <<   " _____      _              __     _______                    " << endl;
+    cout <<   "|_   _|   _| |_ ___  _ __  \ \   / /___ /                    " << endl;
+    cout <<   "  | || | | | __/ _ \| '__|  \ \ / /  |_ \                    " << endl;
+    cout <<   "  | || |_| | || (_) | |      \ V /  ___) |                   " << endl;
+    cout <<   "  |_| \__,_|\__\___/|_|       \_/  |____/                    " << endl;
+    cout <<   "*************************************************************" << endl;
+    cout << "Math is good for your brain." << endl;
+    cout << "So lets feed your brain some math!" << endl << endl;
+    cout << "What is your name my pupil? :";
+    getline(cin, name); // gets the users name
+    cout << "Hello ";
+    cout << name << ". Lets learn some math!!!" << endl;
     // This is the introduction of the program before the math. Only one string value used.
     //
     do {
-    // generating the random numbers and math type
-    leftn = std::rand() % 10 +1; // random value for left number
-    rightn = std::rand() % 10 +1; // random value for the right number
-    mathType = static_cast<MathType>(rand() % 4); // random variable for math operator
+        // generating the random numbers and math type
+        leftn = std::rand() % 10 +1; // random value for left number
+        rightn = std::rand() % 10 +1; // random value for the right number
+        mathType = static_cast<MathType>(rand() % 4); // random variable for math operator
 
-    // if statement for the math type and question
+        // if statement for the math type and question
 
-        switch (mathType){
-            case MT_ADD: // case for addition
-                correctAnswer = (leftn + rightn);
-                cout << leftn << "+" << rightn <<"=";
-                cin >> userAnswer;
-                break;
-
-            case MT_SUB: // case for subtraction
-                if (rightn > leftn) {
-                    correctAnswer =(rightn - leftn);
-                    cout << rightn <<"-" <<leftn <<"=";
-                    cin >> userAnswer;
-                }
-
-
-                else {
-                    correctAnswer = (leftn - rightn);
-                    cout << leftn <<"-" <<rightn <<"=";
-                    cin >> userAnswer;
+        switch (mathType) {
+            case MT_ADD:
+                correctAnswer = leftn + rightn;
+                cout << leftn << " + " << rightn;
+                cout << " = ";
+                while (!(cin >> userAnswer)) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "\tInvalid input!" << endl;
+                    cout << "\tPlease enter a number: ";
                 }
                 break;
 
-            case MT_MUL: // case for multiplication
-                correctAnswer =(rightn * leftn);
-                cout << rightn <<"*" <<leftn <<"=";
-                cin >> userAnswer;
-                break;
-
-            case MT_DIV: // case for division
-                if (rightn > leftn) {
-                    correctAnswer =(rightn / leftn);
-                    cout << rightn <<"/" <<leftn <<"=";
-                    cin >> userAnswer;
-                }
 
 
-                else {
-                    correctAnswer = (leftn / rightn);
-                    cout << leftn <<"/" <<rightn <<"=";
-                    cin >> userAnswer;
+            case MT_SUB:
+                correctAnswer = leftn + rightn;
+                cout << leftn << " + " << rightn;
+                cout << " = ";
+                while (!(cin >> userAnswer)) {
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    cout << "\tInvalid input!" << endl;
+                    cout << "\tPlease enter a number: ";
                 }
                 break;
 
-            default: // if the code breaks
-                cout << "Display error -1" << endl << "contact DaneMcClary on Github";
-                break;
 
+                if (correctAnswer = (leftn - rightn)) {
+                    cout << leftn <<"-" << rightn;
+                    cout << " = ";
+                    while (!(cin >> userAnswer)) {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "\tInvalid input!" << endl;
+                        cout << "\tPlease enter a number: ";
+                    }
+                    break;
+
+
+                    case MT_MUL:
+                    correctAnswer = leftn + rightn;
+                    cout << leftn << " + " << rightn;
+                    cout << " = ";
+                    while (!(cin >> userAnswer)) {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "\tInvalid input!" << endl;
+                        cout << "\tPlease enter a number: ";
+                    }
+                    break;
+
+
+
+                    case MT_DIV:
+                    correctAnswer = leftn + rightn;
+                    cout << leftn << " + " << rightn;
+                    cout << " = ";
+                    while (!(cin >> userAnswer)) {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "\tInvalid input!" << endl;
+                        cout << "\tPlease enter a number: ";
+                    }
+                    break;
+
+
+                    if (correctAnswer = (leftn / rightn)) {
+                        cout << leftn <<"/" << rightn;
+                        cout << " = ";
+                        while (!(cin >> userAnswer)) {
+                            cin.clear();
+                            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                            cout << "\tInvalid input!" << endl;
+                            cout << "\tPlease enter a number: ";
+                        }
+                        break;
+
+                        default: // if the code breaks
+                        cout << "Display error -1" << endl << "contact DaneMcClary on Github";
+                        break;
+
+                    }
+                    // end of the get userAnswer while loop
+                    if (userAnswer == correctAnswer) {
+                        cout << endl << "Your answer is correct." << endl;
+                    }else {
+                        cout << endl << "The correct number is " << correctAnswer << endl;
+
+
+
+
+                        //  cout << endl << endl;
+                        //cout << "What is " << leftn << " + " << rightn << "?" << endl;
+                        // Asking the big question
+                        // cin >> answer;
+                        // Getting an answer for the question
+                        //   cout << "Thank you for using our beta version of Math Tutor! More is on the way." << endl;
+
+                    }
+                }
         }
 
-
-    if (userAnswer == correctAnswer) {
-        cout << endl << "Your answer is correct." << endl;
-    }
-    else {
-        cout << endl << "The correct number is " << correctAnswer << endl;
-    }
-    }while (userChoice == "yes" || userChoice == "y");
-
-
-
-
-
-  //  cout << endl << endl;
-    //cout << "What is " << leftn << " + " << rightn << "?" << endl;
-    // Asking the big question
-   // cin >> answer;
-    // Getting an answer for the question
- //   cout << "Thank you for using our beta version of Math Tutor! More is on the way." << endl;
-
-
+    } while (userChoice == "yes" || userChoice == "y");
     return 0;
 }
 
